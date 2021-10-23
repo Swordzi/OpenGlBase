772e25d6b4a4422b64e3c474545b79fa055cf8e9
Swordzi <86320186+Swordzi@users.noreply.github.com>
2021-10-23 16:40:05 +0300

    Kolmio
    Se piirtää kolmion lol

--- Main.cpp
+++ Main.cpp
@@ -2,19 +2,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-using namespace std;
 
 
-void resize(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
 
-void render(GLFWwindow* window) {
 
-   
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0 in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x; aPos.y, aPos.z, 1.0); \n"
+"}\0";
+const char* fragmentShaderSource = "#version 330 core \n"
+"out vec4 FragColor; \n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f); \n"
+"}\n\0";
 
-}
 
 int main() {
 
@@ -29,7 +33,13 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //tell GLFW we are using core profile
-    
+    GLfloat vertices[] =
+    {
+       -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+       0.5f, -0.5f * float(sqrt(3)) / 3,0.0f,
+       0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+    };
+
 
     //Create a GLFWWindow size of the integers chosen. Also Picking a name for the window
     GLFWwindow* window = glfwCreateWindow(width, height, "Window Testing", NULL, NULL);
@@ -41,17 +51,65 @@ int main() {
     glfwMakeContextCurrent(window);
     gladLoadGL();
 
+    glViewport(0, 0, width, height);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+
+    GLuint VAO, VBO;
+
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    glClearColor(0.7f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
     
-    glfwSetFramebufferSizeCallback(window, resize);
 
 
     //Keep the window open
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.7f, 0.13f, 0.17f, 1.0f);
-        render(window);
-        glfwSwapBuffers(window);
         glfwPollEvents();
+        glClearColor(0.7f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
